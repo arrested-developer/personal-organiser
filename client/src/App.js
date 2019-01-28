9dfab04f1f671ba4b21957d63b43378d0e591f39
Michael Watts <hello@michaelwatts.co>
2019-01-28 21:55:12 +0000

    initialise project

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { message: 'Not set' };
+  }
+  componentDidMount() {
+    axios.get('/api').then(response => {
+      this.setState({ message: response.data });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>React App with Express</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {this.state.message}
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
