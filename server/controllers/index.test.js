b3899822d9a574baa3b42eabc7d1cfad492a48f0
Michael Watts <hello@michaelwatts.co>
2019-01-28 23:03:38 +0000

    add server route test

---
+++ server/controllers/index.test.js
@@ -0,0 +1,15 @@
+const test = require('tape');
+const request = require('supertest');
+
+const router = require('../app.js');
+
+test('api/message should return 200', t => {
+  t.plan(1);
+  request(router)
+    .get('/api/message/')
+    .expect(200)
+    .then(res => {
+      t.assert(res.text, 'Message from Express', 'Gives expected message');
+      t.end();
+    });
+});
