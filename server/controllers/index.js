7036ed17fe706c52126171551c9c82f4b2667d0c
Michael Watts <hello@michaelwatts.co>
2019-01-29 00:37:45 +0000

    add prettier and eslint

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express")
+const router = express.Router()
 
 /* GET home page. */
-router.get('/message/', function(req, res, next) {
-  res.status(200).send('Message from Express');
-});
+router.get("/message/", function(req, res, next) {
+  res.status(200).send("Message from Express")
+})
 
-module.exports = router;
+module.exports = router
